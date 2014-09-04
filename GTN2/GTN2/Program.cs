8655ec30b2ce955ec1690696fd03e8f819f5f20e
Daimeon DD Wren <daimeonddwren@gmail.com>
2014-09-04 13:10:04 -0600

    GTN

---
+++ GTN2/GTN2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTN2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void GTN()
+        {
+            Random rng = new Random();
+            int compNum = rng.Next(1, 101);
+            int guesses = 1;
+            bool guessing = true;
+
+
+            while (guessing)
+            {
+                string zinput = Console.ReadLine();
+                int playerguess = int.Parse(zinput);
+                if (playerguess > compNum)
+                {
+                    guesses++;
+                    Console.WriteLine("Your guess was a bit to high");
+                }
+                else if (playerguess < compNum)
+                {
+                    guesses++;
+                    Console.WriteLine("Your guess was a bit to low");
+                }
+                else
+                {
+                    guessing = false;
+                    Console.WriteLine("Wow! Great guess, you got me.");
+                }
+
+            }
+        }
+    }
+}
---
+++ GuessThatNumber/GuessThatNumber/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessThatNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Promt the user for input
+            Console.WriteLine("Please input the dice to roll & the number of sides e.g. \"5d6\" ");
+            // We call our roll die function
+            RollDie(Console.ReadLine());
+            // Keep the console open so we may review 
+            GTN();
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Rolls a die that specified by the input
+        /// </summary>
+        /// <param name="input"></param>
+        static void RollDie(string input)
+        {
+            // split the input into our roll count and die sides
+            string[] splitIn = input.Split(' ');
+            for (int j = 0; j < splitIn.Length; j++)
+            {
+                string[] split = splitIn[j].Split('d');
+                // we parse the the first index of split into an int
+                int rollCount = int.Parse(split[0]);
+                // we parse the second index into our die sides 
+                int dsides = int.Parse(split[1]);
+                int avg = 0;
+                // we declare a a random number generator 
+                Random rng = new Random();
+                // An empty string for output
+                string output = string.Empty;
+                // we loop through number of roll counts
+                for (int i = 0; i < rollCount; i++)
+                {
+                    // we add the roll to our output
+                    int roll = rng.Next(1, dsides + 1);
+                    output += roll + " ";
+                    avg += roll;
+
+
+                    //outputing output
+                }
+                avg = avg / rollCount;
+                Console.WriteLine("\n" + output + "\nAverage of rolls: " + avg);
+            }
+
+        }
+        static void GTN()
+        {
+            Random rng = new Random();
+            int compNum = rng.Next(1, 101);
+            int guesses = 1;
+            bool guessing = true;
+           
+
+            while (guessing)
+            {
+                string zinput = Console.ReadLine();
+                int playerguess = int.Parse(zinput);
+                if (playerguess > compNum)
+                {
+                    guesses++;
+                    Console.WriteLine("Your guess was a bit to high");
+                }
+                else if (playerguess < compNum)
+                {
+                    guesses++;
+                    Console.WriteLine("Your guess was a bit to low");
+                }
+                else
+                {
+                    guessing = false;
+                    Console.WriteLine("Wow! Great guess, you got me.");
+                }
+
+
+
+            }
+        }
+    }
+}
+
