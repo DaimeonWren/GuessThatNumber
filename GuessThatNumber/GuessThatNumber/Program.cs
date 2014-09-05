521aaa972159f4c90000b218b00e88eda2670673
Daimeon DD Wren <daimeonddwren@gmail.com>
2014-09-05 08:25:27 -0600

    .2

--- GuessThatNumber/GuessThatNumber/Program.cs
+++ GuessThatNumber/GuessThatNumber/Program.cs
@@ -55,38 +55,7 @@ namespace GuessThatNumber
             }
 
         }
-        static void GTN()
-        {
-            Random rng = new Random();
-            int compNum = rng.Next(1, 101);
-            int guesses = 1;
-            bool guessing = true;
-           
-
-            while (guessing)
-            {
-                string zinput = Console.ReadLine();
-                int playerguess = int.Parse(zinput);
-                if (playerguess > compNum)
-                {
-                    guesses++;
-                    Console.WriteLine("Your guess was a bit to high");
-                }
-                else if (playerguess < compNum)
-                {
-                    guesses++;
-                    Console.WriteLine("Your guess was a bit to low");
-                }
-                else
-                {
-                    guessing = false;
-                    Console.WriteLine("Wow! Great guess, you got me.");
-                }
-
-
-
-            }
-        }
+   
     }
 }
 
